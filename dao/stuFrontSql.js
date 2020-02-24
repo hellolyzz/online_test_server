c89723e2f22555284acd3214dee52907de8ef12c
liyuanzhu <997071697@qq.com>
2020-02-24 19:20:05 +0800

    完成学生部分的信息设置部分

--- dao/stuFrontSql.js
+++ dao/stuFrontSql.js
@@ -1,7 +1,17 @@
 module.exports = {
+  // 获取学生试卷
   getTestInfo(params,queryInfo){
     var offset = (queryInfo.pagenum - 1) * queryInfo.pagesize;
     return sql = [`select * from tb_testmanage where institute = '${params.institute}' and major = '${params.major}' and grade = ${params.grade} limit ${offset}, ${queryInfo.pagesize}`, 
     `select count(*) as total from tb_testmanage where institute = '${params.institute}' and major = '${params.major}' and grade = ${params.grade}`]
+  },
+   // 查询单个学生信息
+   getInfoById(id){
+    var sql = `select * from tb_student where id = ${id}`
+    return sql;
+  },
+  // 修改学生信息
+  editStuInfo(params){
+    return sql = `update tb_student set tel = '${params.tel}', email = '${params.email}' , pwd = '${params.pwd}' where id = ${params.id}`
   }
 }
--- routes/stuFront.js
+++ routes/stuFront.js
@@ -4,6 +4,7 @@ var $sql = require('../dao/stuFrontSql')
 var util = require('../util/util')
 var publicDao = require('../dao/publicDao')
 
+// 获取该学生试卷
 router.post('/getTestInfo',function(req, res){
   var queryInfo = req.body.params.queryInfo
   var params = req.body.params.params;
@@ -32,6 +33,62 @@ router.post('/getTestInfo',function(req, res){
       meta:{
         message: '获取所有试卷成功',
         meta: 200
+      }
+    })
+  })
+})
+
+// 获取学生信息byid
+router.get('/getInfoById/:id', function (req, res) {
+  var id = req.params.id;
+  // console.log(id)
+  var sql = $sql.getInfoById(id);
+  publicDao.Query(sql, function (err, data) {
+    if (err) {
+      return res.send({
+        data: null,
+        meta: {
+          status: 401,
+          message: '获取个人信息失败'
+        }
+      })
+    }
+    var dataStr = JSON.stringify(data);
+    var data = JSON.parse(dataStr)
+    // console.log(data)
+    // console.log(util.arrObj({},data))
+    res.send({
+      data: util.arrObj({}, data),
+      meta: {
+        status: 200,
+        message: '获取个人信息成功'
+      }
+    })
+  })
+})
+
+// 修改学生信息
+router.put('/editStu', function (req, res) {
+  var params = req.body.params;
+  // console.log(params)
+  var sql = $sql.editStuInfo(params);
+  // console.log(sql)
+  publicDao.Query(sql, function (err, data) {
+    if (err) {
+      return res.send({
+        data: null,
+        meta: {
+          status: 401,
+          message: '修改个人信息失败'
+        }
+      })
+    }
+    // console.log(data)
+    res.send({
+      data: util.arrObj({}, data),
+      meta: {
+        status: 200,
+        message: '修改个人信息成功'
       }
     })
   })
